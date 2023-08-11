be208e1d00ee79d67e1b41f09a0720c90ec7ab61
DIMs <DIMs@DIM-HOME>
2023-08-11 17:22:45 +0300

    Add project files.

---
+++ CSharpStepikHomework12-1-13/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpStepikHomework12_1_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
