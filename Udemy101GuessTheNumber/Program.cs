a29232bd38a427099c65b9e754d68eb4df310ab7
zavadimka <che.zavada@gmail.com>
2023-08-16 08:32:01 +0300

    Added Udemy homeworks #101 and #103

---
+++ Udemy101GuessTheNumber/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace Udemy101GuessTheNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует игра \"Угадай число\"!");
+            Console.WriteLine("Один игрок загадывает число, а другой должен его отгадать.");
+            Console.Write("Хотите, чтобы компьютер загадал число? \"Да\" или \"Нет\": ");
+
+            string isVSComputer = Console.ReadLine();
+
+            switch (isVSComputer)
+            {
+                case "Да":
+                    StartGamePlayeerAnswer();
+                    break;
+                case "Нет":
+                    StartGameAIAnswer();
+                    break;
+                default:
+                    throw new ArgumentException("Введён некорректный ответ");
+            }
+        }
+
+
+        private static int GetNumber()
+        {
+            Random random = new Random();
+                
+            return random.Next(0, 100);
+        }
+
+        private static void StartGamePlayeerAnswer()
+        {
+            int number = GetNumber();
+            int answer;
+            bool isRigthAnswer = false;
+
+            Console.WriteLine("Компьютер загадал число от 0 до 100. У Вас есть 5 попыток, чтобы его отгадать");
+
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write("Ваш ответ: ");
+                answer = int.Parse(Console.ReadLine());
+
+                if (answer == number)
+                {
+                    Console.WriteLine("Верно, Вы выиграли!");
+                    isRigthAnswer = true;
+                    break;
+                }
+                else if (answer < number)
+                {
+                    Console.WriteLine($"Ответ неверный, осталось попыток: {5 - i}");
+                    Console.WriteLine($"{answer} меньше загаданного числа");
+                }
+                else
+                {
+                    Console.WriteLine($"Ответ неверный, осталось попыток: {5 - i}");
+                    Console.WriteLine($"{answer} больше загаданного числа");
+                }
+            }
+
+            if (!isRigthAnswer)
+            {
+                Console.WriteLine($"Вы проиграли. Правльный ответ: {number}");
+            }
+        }
+        
+
+        static void StartGameAIAnswer()
+        {
+            int number = -1;
+            int firstNumber = 0;
+            int lastNumber = 100;
+            int answer;
+            bool isRigthAnswer = false;
+
+            
+            while (number < 0 || number > 100)
+            {
+                Console.Write("Загадайте число от 0 до 100: ");
+                number = int.Parse(Console.ReadLine());
+            }
+            
+            
+            Console.WriteLine("У ИИ есть 5 попыток, чтобы его отгадать");
+            answer = (lastNumber - firstNumber) / 2;
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine($"Ответ ИИ: {answer}");
+
+                if (answer == number)
+                {
+                    Console.WriteLine("Верно, ИИ выиграл!");
+                    isRigthAnswer = true;
+                    break;
+                }
+                else if (answer < number)
+                {
+                    Console.WriteLine($"Ответ неверный, осталось попыток: {5 - i}");
+                    Console.WriteLine($"{answer} меньше загаданного числа");
+
+                    firstNumber = answer;
+                    answer =  answer + (lastNumber - firstNumber) / 2;
+                }
+                else
+                {
+                    Console.WriteLine($"Ответ неверный, осталось попыток: {5 - i}");
+                    Console.WriteLine($"{answer} больше загаданного числа");
+
+                    lastNumber = answer;
+                    answer = answer - (lastNumber - firstNumber) / 2;
+                }
+            }
+
+            Console.WriteLine();
+            if (!isRigthAnswer)
+            {
+                Console.WriteLine($"ИИ проиграл. Правльный ответ: {number}");
+            }
+        }
+    }
+}
---
+++ Udemy103TickTackToe/Program.cs
@@ -0,0 +1,190 @@
+﻿namespace Udemy103TickTackToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] gameBoard = new char[3, 3]
+            {
+                { ' ', ' ', ' ' },
+                { ' ', ' ', ' ' },
+                { ' ', ' ', ' ' }
+            };
+
+            char[,] gameBoardNum = new char[3, 3]
+            {
+                { '0', '1', '2' },
+                { '3', '4', '5' },
+                { '6', '7', '8' }
+            };
+
+            Console.WriteLine("Вас приветствует игра \"Крестики-Нолики\"!");
+            Console.WriteLine("Первыми ходят крестики. Удачи!");
+            
+            PrintGameBoard();
+
+            char cross = 'X';
+            string krestikami = "крестиками";
+            
+            char nought = 'O';
+            string nolikami = "ноликами";
+
+            string fullLine = "012345678";
+            string matchResult = null;
+
+            bool isWin = false;
+            while (!isWin || matchResult != "draw")
+            {
+                MakeTurn(cross, krestikami);
+                PrintGameBoard();
+                isWin = CheckWinner(gameBoard, out string result1);
+                matchResult = result1;
+                if (isWin)
+                {
+                    Console.WriteLine("Поздравляем! Победили крестики!");
+                    break;
+                }
+                else if (matchResult == "draw")
+                {
+                    Console.WriteLine("Ничья! Победила дружба!");
+                    break;
+                }
+
+                MakeTurn(nought, nolikami);
+                PrintGameBoard();
+                isWin =  CheckWinner(gameBoard, out string result2);
+                matchResult = result2;
+                if (isWin)
+                {
+                    Console.WriteLine("Поздравляем! Победили нолики!");
+                    break;
+                }
+                else if (matchResult == "draw")
+                {
+                    Console.WriteLine("Ничья! Победила дружба!");
+                    break;
+                }
+            }
+            
+
+
+
+            void PrintGameBoard()
+            {
+                Console.WriteLine();
+                Console.Write($" {gameBoard[0, 0]} | {gameBoard[0, 1]} | {gameBoard[0, 2]} ");
+                Console.Write("     ");
+                Console.WriteLine($" {gameBoardNum[0, 0]} | {gameBoardNum[0, 1]} | {gameBoardNum[0, 2]} ");
+
+                Console.Write($"---|---|---");
+                Console.Write("     ");
+                Console.WriteLine($"---|---|---");
+
+                Console.Write($" {gameBoard[1, 0]} | {gameBoard[1, 1]} | {gameBoard[1, 2]} ");
+                Console.Write("     ");
+                Console.WriteLine($" {gameBoardNum[1, 0]} | {gameBoardNum[1, 1]} | {gameBoardNum[1, 2]} ");
+
+                Console.Write($"---|---|---");
+                Console.Write("     ");
+                Console.WriteLine($"---|---|---");
+
+                Console.Write($" {gameBoard[2, 0]} | {gameBoard[2, 1]} | {gameBoard[2, 2]} ");
+                Console.Write("     ");
+                Console.WriteLine($" {gameBoardNum[2, 0]} | {gameBoardNum[2, 1]} | {gameBoardNum[2, 2]} ");
+                Console.WriteLine();
+            }
+
+            void MakeTurn(char thisTurnSymbol, string thisTurnValue)
+            {
+                string position = " ";
+
+                while (!fullLine.Contains(position))
+                {
+                    Console.Write($"Ход {thisTurnValue}. Введите номер поля: ");
+                    position = Console.ReadLine();
+                }
+
+                switch (position)
+                {
+                    case "0":
+                        gameBoard[0, 0] = thisTurnSymbol;
+                        gameBoardNum[0, 0] = ' ';
+                        break;
+                    case "1":
+                        gameBoard[0, 1] = thisTurnSymbol;
+                        gameBoardNum[0, 1] = ' ';
+                        break;
+                    case "2":
+                        gameBoard[0, 2] = thisTurnSymbol;
+                        gameBoardNum[0, 2] = ' ';
+                        break;
+                    case "3":
+                        gameBoard[1, 0] = thisTurnSymbol;
+                        gameBoardNum[1, 0] = ' ';
+                        break;
+                    case "4":
+                        gameBoard[1, 1] = thisTurnSymbol;
+                        gameBoardNum[1, 1] = ' ';
+                        break;
+                    case "5":
+                        gameBoard[1, 2] = thisTurnSymbol;
+                        gameBoardNum[1, 2] = ' ';
+                        break;
+                    case "6":
+                        gameBoard[2, 0] = thisTurnSymbol;
+                        gameBoardNum[2, 0] = ' ';
+                        break;
+                    case "7":
+                        gameBoard[2, 1] = thisTurnSymbol;
+                        gameBoardNum[2, 1] = ' ';
+                        break;
+                    case "8":
+                        gameBoard[2, 2] = thisTurnSymbol;
+                        gameBoardNum[2, 2] = ' ';
+                        break;
+                }
+
+                fullLine = fullLine.Remove(fullLine.IndexOf(position), 1);
+            }
+
+
+            bool CheckWinner(char[,] gameBoard, out string matchResult)
+            {
+                matchResult = null;
+                bool isWin = false;
+
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (gameBoard[i, 0] != ' ' && gameBoard[i, 0] == gameBoard[i, 1] && gameBoard[i, 0] == gameBoard[i, 2])
+                    {
+                        isWin = true;
+                        break;
+                    }
+                }
+                
+                for (int j = 0; j < 3; j++)
+                {
+                    if (gameBoard[0, j] != ' ' && gameBoard[0, j] == gameBoard[1, j] && gameBoard[0, j] == gameBoard[2, j])
+                    {
+                        isWin = true;
+                        break;
+                    }
+                }
+
+                if (gameBoard[0, 0] != ' ' && gameBoard[0, 0] == gameBoard[1, 1] && gameBoard[0, 0] == gameBoard[2, 2] ||
+                    (gameBoard[0, 2] != ' ' && gameBoard[0, 2] == gameBoard[1, 1] && gameBoard[0, 2] == gameBoard[2, 0]))
+                {
+                    isWin = true;
+                }
+
+                if (!isWin && fullLine == "")
+                {
+                    matchResult = "draw";
+                }
+
+                return isWin;
+            }
+        }
+    }
+}
