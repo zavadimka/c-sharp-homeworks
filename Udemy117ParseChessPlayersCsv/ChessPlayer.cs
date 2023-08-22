8519c25cf6d1507c3a5a05abcef78d2d60137b56
zavadimka <che.zavada@gmail.com>
2023-08-22 19:45:19 +0300

    Added Udemy117ParseChessPlayersCsv

---
+++ Udemy117ParseChessPlayersCsv/ChessPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy117ChessPlayers
+{
+    public class ChessPlayer
+    {
+        public int Rank { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Country { get; private set; }
+        public int Rating { get; private set; }
+        public int BirthYear { get; private set; }
+
+
+        public static ChessPlayer ParseChessPlayersCsv(string chessPlayerString)
+        {
+            string[] data = chessPlayerString.Split(';');
+
+            ChessPlayer player = new ChessPlayer()
+            {
+                Rank = int.Parse(data[0]),
+                FirstName = data[1].Split(',')[1].Trim(),
+                LastName = data[1].Split(',')[0].Trim(),
+                Country = data[3],
+                Rating = int.Parse(data[4]),
+                BirthYear = int.Parse(data[6]),
+            };
+
+            return player;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rank} {FirstName} {LastName} {Country} {Rating} {BirthYear}";
+        }
+
+
+    }
+}
---
+++ Udemy117ParseChessPlayersCsv/Program.cs
@@ -0,0 +1,27 @@
+﻿using Udemy117ChessPlayers;
+
+namespace Udemy117ChessGamers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetPlayersByCondition("Top100ChessPlayers.csv");
+        }
+
+        static void GetPlayersByCondition (string file)
+        {
+            var players = File.ReadAllLines(file)
+                                            .Skip(1)
+                                            .Select(ChessPlayer.ParseChessPlayersCsv)
+                                            .Where(player => player.Country == "RUS" && player.Rating > 2700)
+                                            .OrderBy(player => player.BirthYear)
+                                            .ToList();
+
+            foreach (var player in players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
